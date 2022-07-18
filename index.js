20ebf94f5108501ce941fc53d9745b9aa5bb9289
islomjon6975 <islomjon0919@mail.ru>
2022-07-18 08:49:12 +0500

    s

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const http = require("http");
+const {v4} = require("uuid");
+const getBodyData = require("./util");
+
 
 const books = [
     {
@@ -7,9 +10,9 @@ const books = [
         pages: 250,
         author: 'Writer 1'
     }
-]
+];
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
     // Get all books
     if(req.url === "/books" && req.method === "GET") {
         res.writeHead(200, {"Content-Type": "application/json charset=utf8"})
@@ -17,10 +20,58 @@ const server = http.createServer((req, res) => {
             status: "OK",
             books
         }
-    
-        req.end(JSON.stringify(resp));
+        res.end(JSON.stringify(resp));
     } else if(req.url === "/books" && req.method === "POST") {
-
+        const data = await getBodyData(req);
+        const { title, pages, author} = JSON.parse(data);
+        const newBook = {
+            id: v4(),
+            title,
+            pages, 
+            author
+        }
+        books.push(newBook);
+        const resp = {
+            status: 'Created',
+            book: newBook
+        }
+        res.writeHead(200, {"Content-Type": "application/json charset=utf8"})
+        res.end(JSON.stringify(resp))
+    } else if(req.url.match(/\/books\/\w+/) && req.method === 'GET') {
+        const id = req.url.split('/')[2];
+        const book = books.find(b => b.id === id)
+        res.writeHead(200, {"Content-Type": "application/json charset=utf8"})
+        const resp = {
+            status: 'OK',
+            book
+        }
+        res.end(JSON.stringify(resp))
+    } else if(req.url.match(/\/books\/\w+/) && req.method === 'PUT') {
+        const id = req.url.split('/')[2];
+        const data = await getBodyData(req);
+        const { title, pages, author} = JSON.parse(data);
+        const idx = books.findIndex(b => b.id === id);
+        const changedBook = {
+            id: books[idx].id,
+            title: title || books[idx].title,
+            pages: pages || books[idx].pages,
+            author: author || books[idx].author
+        }
+        books[idx] = changedBook
+        res.writeHead(200, {"Content-Type": "application/json charset=utf8"})
+        const resp = {
+            status: 'OK',
+            book: changedBook
+        }
+        res.end(JSON.stringify(resp))
+    } else if(req.url.match(/\/books\/\w+/) && req.method === 'DELETE') {
+        const id = req.url.split('/')[2];
+        books = books.filter(b => b.id !== id);
+        res.writeHead(200, {"Content-Type": "application/json charset=utf8"})
+        const resp = {
+            status: 'Deleted',
+        }
+        res.end(JSON.stringify(resp))
     }
 });
 
